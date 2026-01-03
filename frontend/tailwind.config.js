7af8182bdf227ad44b50563731e888cbc294bdef
Gokul-Gopi <gokulg1412@gmail.com>
2026-01-03 15:09:13 +0000

    Add Tailwind CSS and PostCSS configuration; update styles in toolbar and index.css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#ffffff;",
+          100: "#fce4ff;",
+          200: "#d6beff",
+          300: "#b199ff",
+          400: "#8b73ec",
+          500: "#6e56cf",
+          600: "#3f27a0",
+          700: "#1a027b",
+          800: "#000055",
+          900: "#00002f",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
